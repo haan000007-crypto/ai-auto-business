431114b76dc6df1f1709a7550123884c2c1c93ce
haan000007-crypto <haan000007@gmail.com>
2026-08-21 16:36:42 +0700

    feat: register AI command module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module.js";
+import { CommandModule } from "./commands/command.module.js";
 import { DatabaseModule } from "./database/database.module.js";
 import { RbacModule } from "./rbac/rbac.module.js";
 import { WorkflowsModule } from "./workflows/workflows.module.js";
 
 @Module({
-  imports: [DatabaseModule, AuthModule, RbacModule, WorkflowsModule],
+  imports: [DatabaseModule, AuthModule, RbacModule, WorkflowsModule, CommandModule],
 })
 export class AppModule {}
